41c100557a4441338c3c509432a0770dc81bd4d5
mouad4949 <mouadrguibi900@gmail.com>
2024-10-28 16:17:55 +0100

    supp nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 
 import Hero from "../components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
-import { navItems } from "@/data";
+
 import Technologies from "@/components/Technologies";
 import Deployment from "@/components/Deployment";
 import { TimelineDemo } from "@/components/Experiences";
@@ -16,7 +14,7 @@ export default function Home() {
   return (
     <main className="relative overflow-clip bg-black-100 flex justify-center items-ceter flex-col  mx-auto sm:px-10 px-5">
       <div className="maw-w-7xl w-full">
-        <FloatingNav navItems={navItems}/>
+        
         <Hero/>
         <Grid/>
         <RecentProjects/>
