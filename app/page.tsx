2009d91962c53868f1ec3f4baabe19ef77720fa7
mouad4949 <mouadrguibi900@gmail.com>
2024-10-28 16:25:12 +0100

    supp footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
 
 import Hero from "../components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
-
+import { navItems } from "@/data";
 import Technologies from "@/components/Technologies";
 import Deployment from "@/components/Deployment";
 import { TimelineDemo } from "@/components/Experiences";
 import { Approach } from "@/components/Approach";
-import Footer from "@/components/Footer";
+
 
 
 export default function Home() {
   return (
     <main className="relative overflow-clip bg-black-100 flex justify-center items-ceter flex-col  mx-auto sm:px-10 px-5">
       <div className="maw-w-7xl w-full">
-        
+        <FloatingNav navItems={navItems}/>
         <Hero/>
         <Grid/>
         <RecentProjects/>
@@ -22,7 +24,7 @@ export default function Home() {
         <Deployment/>
         <TimelineDemo/>
         <Approach/>
-        <Footer/>
+        
       </div>
     </main>
   );
