c43793115c7287c99dae7317aa96526a3171e196
mouad4949 <mouadrguibi900@gmail.com>
2024-11-16 15:33:51 +0100

    Bank project

--- data/index.ts
+++ data/index.ts
@@ -72,6 +72,14 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
+      title: "Bank Management Web Application",
+      des: "Developed a bank management app for accounts, transactions, and clients using React and Spring Boot.",
+      img: "/Bank.jpeg",
+      iconLists: ["/spring.svg", "/tail.svg", "/react.svg"],
+      link: "https://github.com/mouad4949/Bank_LSI",
+    },
+    {
+      id: 2,
       title: "Online Booking System for Aeroclub",
       des: "Developed a platform for booking discovery and initiation flights, with automated pricing and secure payment integration.",
       img: "/aeroclub.jpeg",
@@ -79,7 +87,7 @@ export const navItems = [
       link: "https://github.com/mouad4949/Aeroclub_siteweb",
     },
     {
-      id: 2,
+      id: 3,
       title: "Lendys Café SaaS Management System",
       des: "Developed and sold a SaaS platform for managing reservations, orders, and payments at Lendys Café.",
       img: "/lendys.png",
@@ -87,7 +95,7 @@ export const navItems = [
       link: "https://lendys.ma/",
     },
     {
-      id: 3,
+      id: 4,
       title: "E-commerce Platform with PayPal Integration",
       des: "A Symfony-based e-commerce solution featuring seamless PayPal payment integration for a smooth shopping experience.",
       img: "/ecomm.png",
@@ -95,7 +103,7 @@ export const navItems = [
       link: "https://github.com/mouad4949/Ecommerce_WebSite",
     },
     {
-      id: 4,
+      id: 5,
       title: "2D Video Game with Pathfinding",
       des: "A 2D Unity game featuring advanced pathfinding mechanics for dynamic character movement and AI navigation.",
       img: "/game.png",
@@ -103,7 +111,7 @@ export const navItems = [
       link: "https://github.com/mouad4949/Game-DataStructers-",
     },
     {
-      id: 5,
+      id: 6,
       title: "Hotel Management System",
       des: "A web app for managing hotel bookings, billing, and guest services efficiently.",
       img: "/hotel.png",
@@ -111,7 +119,7 @@ export const navItems = [
       link: "https://github.com/mouad4949/Gestion-d-hotel",
     },
     {
-      id: 6,
+      id: 7,
       title: " Gaming Club Management System",
       des: "A web app for managing gaming club memberships, events, and player profiles efficiently.",
       img: "/gaming.png",
